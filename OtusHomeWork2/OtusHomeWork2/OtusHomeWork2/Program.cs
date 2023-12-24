7a1b1ccb5dec28909c7a0c1128ccf9421667c8c2
Беляков Станислав <belyakov.s@universum.ru>
2023-12-24 18:47:24 +0300

    homework is done

--- OtusHomeWork2/OtusHomeWork2/OtusHomeWork2/Program.cs
+++ OtusHomeWork2/OtusHomeWork2/OtusHomeWork2/Program.cs
@@ -1,10 +1,54 @@
-﻿namespace OtusHomeWork2
+﻿using System.Diagnostics;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization.Json;
+using System.Text.Json;
+
+namespace OtusHomeWork2
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
+            TestClass test = new TestClass(1,"gfhfjf", 2, "sdasd");
+            var cnt = 0;
+            Stopwatch sw = Stopwatch.StartNew();
+            while (cnt < 1000)
+            {
+                string json = JsonSerializer.Serialize(test);
+                //Console.WriteLine(json);
+                var restore = JsonSerializer.Deserialize<TestClass>(json);
+                cnt++;
+            }
+            sw.Stop();
+            Console.WriteLine($"Прошло времени: {sw.ElapsedMilliseconds}");
+
+            cnt = 0;
+            sw.Restart();
+            while (cnt < 1000)
+            {
+                Test();
+                cnt++;
+            }
+                sw.Stop();
+            Console.WriteLine($"Прошло времени: {sw.ElapsedMilliseconds}");
+        }
+
+        public static void Test()
+        {
+            var bf = new BinaryFormatter();
+
+            var before = new TestClass(1, "gfhfjf", 2, "sdasd");
+
+            using (var fs = new FileStream("test.txt", FileMode.Create))
+            {
+                bf.Serialize(fs, before);
+            }
+
+            using (var fs = new FileStream("test.txt", FileMode.Open))
+            {
+                var after = (TestClass)bf.Deserialize(fs);
+            }
         }
     }
 }
---
+++ OtusHomeWork2/OtusHomeWork2/OtusHomeWork2/TestClass.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace OtusHomeWork2
+{
+    [Serializable]
+    public class TestClass: ISerializable
+    {
+        public TestClass(int a, string b, int c, string d) 
+        {
+            varOne = a;
+            varTwo = b;
+            varThree = c;
+            varFour = d;
+        }
+        public TestClass() { }
+
+        public int varOne { get; set; }
+
+        public string varTwo { get; set; }
+
+        public int varThree { get; set; }
+
+        public string varFour { get; set; }
+
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            info.AddValue("varOne", this.varOne);
+            info.AddValue("varTwo", this.varTwo);
+            info.AddValue("varThree", this.varThree);
+            info.AddValue("varFour", this.varFour);
+        }
+
+        public TestClass(SerializationInfo info, StreamingContext context)
+        {
+            this.varOne = (int)info.GetValue("varOne", typeof(int));
+            this.varTwo = (string)info.GetValue("varTwo", typeof(string));
+            this.varThree = (int)info.GetValue("varThree", typeof(int));
+            this.varFour = (string)info.GetValue("varFour", typeof(string));
+        }
+
+        public override string ToString()
+        {
+            return $"1: {varOne}; 2: {varTwo}; 3: {varThree}; 4: {varFour};";
+        }
+
+     }
+
+}
